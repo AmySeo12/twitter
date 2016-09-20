c9cdabea769c6b30703562ea4508450ab91d8a32
AmySeo12 <maryori.amy@gmail.com>
2016-09-20 10:52:12 -0500

    subiendo la version 2

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
 window.onload=function() {
+	var contador = document.getElementById("contador");
     var button = document.getElementById("button");
-    button.addEventListener("click", function() {
-    	var textArea = document.getElementById("textArea").value;
+    button.addEventListener("click", function(e) {
+    	e.preventDefault();
+    	var text = document.getElementById("textArea");
+		agregarMensajes(text.value);
+		text.value = "";
+		contador.textContent= "140";
+    });
+
+    function agregarMensajes(texto){
 		var mensajes = document.getElementById("contenedor");
 		var mensaje = document.createElement("div");
-
 		mensaje.classList.add("cuadro");
-		mensaje.innerHTML= textArea;
-
+		mensaje.innerHTML= texto;
 		mensajes.insertBefore(mensaje, mensajes.childNodes[0]);
-		
-		document.getElementById("textArea").value = "";
-    });
-    /*var text = document.getElementById("textArea");
-    textArea.addEventListener("onKeyDown", function(){
-    	var contador = text.value;
-    	var caracteres = 150;
-    	var longitud = contador.longitud;
+    }
+    var max = 140;
+    var publicacion =  document.getElementById("textArea");
+    publicacion.addEventListener("keyup", function(){
+    	var boton = document.getElementById("button");
+    	var longitud = this.value.length;
 
-    	if(longitud <= caracteres){
+    	contadorText(longitud,contador,max);
+
+    	if (this.value.length >0 )
+    		boton.disabled= false;
+    	else
+    		boton.disabled=true;
     });
-*/
+
+    function contadorText(longitud, contador, max){
+    	if(longitud <= max)
+    		contador.textContent = max - longitud;
+    	else
+    		contador.textContent = max -longitud;
+    }
 }
